c89902452751a3637f3cc9c7549917238f482352
viktoriiakychan <viktoriia.kychan@gmail.com>
2022-06-20 10:48:36 +0100

    Skip pages

--- WpfApptEST/ListUsersWindow1.xaml.cs
+++ WpfApptEST/ListUsersWindow1.xaml.cs
@@ -90,18 +90,26 @@ namespace WpfApptEST
                 context.SaveChanges();
             }
         }
+        int page = 1; // 2 - another page
+        IQueryable<User> query;
+        List<User> result;
+        int skip;
+        int pageSize = 10;
         ObservableCollection<User> collection = null;
+        int pages;
+        ChooseFieldsToSearchWindow choose = new ChooseFieldsToSearchWindow();
         private void btnSeach_Click(object sender, RoutedEventArgs e)
         {
-           //MessageBox.Show(AuthUser.UserEmail);
-            ChooseFieldsToSearchWindow choose = new ChooseFieldsToSearchWindow();
+            //MessageBox.Show(AuthUser.UserEmail);
+            
             choose.ShowDialog();
+            //collection.Clear();
             string name = choose.Name;
+            MyDataContext context = new MyDataContext();
+            query = context.Users.AsQueryable();
             string surname = choose.Surname;
             string email = choose.Email;
             string phone = choose.Phone;
-            MyDataContext context = new MyDataContext();
-            var query = context.Users.AsQueryable();
             if(!string.IsNullOrWhiteSpace(name))
             {
                 query = query.Where(x => x.Name.Contains(name));
@@ -118,11 +126,20 @@ namespace WpfApptEST
             {
                 query = query.Where(x => x.Phone.Contains(phone));
             }
-            //else
-            //{
-            //    query = context.Users.AsQueryable();
-            //}
-            var result = query.ToList();
+            else
+            {
+                query = context.Users.AsQueryable();
+            }
+            //page 1
+
+
+           
+            skip = (page - 1) * pageSize;
+            int count = query.Count();
+            pages = (int)Math.Ceiling((double)count / (double)pageSize);
+            MessageBox.Show("Pages =>" + pages);
+            query = query.Skip(skip).Take(10);
+            result = query.ToList();
             //collection.Clear();
             if (collection == null)
             {
@@ -138,12 +155,124 @@ namespace WpfApptEST
             {
                 collection.Add(new User() { Id = i + 1, Name =  result[i].Name, Email = result[i].Email, Image = result[i].Image, Surname = result[i].Surname, Password = result[i].Password, Phone = result[i].Phone});
             }
-           
+            
         }
 
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
             collection.Clear();
+        }
+
+        private void forward_Click(object sender, RoutedEventArgs e)
+        {
+            if (page < pages)
+            {
+                page += 1;
+                collection.Clear();
+                string name = choose.Name;
+                MyDataContext context = new MyDataContext();
+                query = context.Users.AsQueryable();
+                string surname = choose.Surname;
+                string email = choose.Email;
+                string phone = choose.Phone;
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    query = query.Where(x => x.Name.Contains(name));
+                }
+                else if (!string.IsNullOrWhiteSpace(surname))
+                {
+                    query = query.Where(x => x.Surname.Contains(surname));
+                }
+                else if (!string.IsNullOrWhiteSpace(email))
+                {
+                    query = query.Where(x => x.Email.Contains(email));
+                }
+                else if (!string.IsNullOrWhiteSpace(phone))
+                {
+                    query = query.Where(x => x.Phone.Contains(phone));
+                }
+                else
+                {
+                    query = context.Users.AsQueryable();
+                }
+                skip = (page - 1) * pageSize;
+                //collection.Clear();
+                query = query.Skip(skip).Take(10);
+                result = query.ToList();
+                //collection = new ObservableCollection<User>();
+                if (collection == null)
+                {
+                    collection = new ObservableCollection<User>();
+                    dgUsers.ItemsSource = collection;
+                }
+                List<string> imageList = new List<string>();
+                if (result.Count == 0)
+                {
+                    MessageBox.Show("0 person founded");
+                }
+                for (int i = 0; i < result.Count; i++)
+                {
+                    collection.Add(new User() { Id = i + 1, Name = result[i].Name, Email = result[i].Email, Image = result[i].Image, Surname = result[i].Surname, Password = result[i].Password, Phone = result[i].Phone });
+                }
+            }
+            else if(page-1 == pages)
+            {
+                forward.IsEnabled = false;
+            }
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            if (page > 1)
+            {
+                page -= 1;
+
+
+                collection.Clear();
+                string name = choose.Name;
+                MyDataContext context = new MyDataContext();
+                query = context.Users.AsQueryable();
+                string surname = choose.Surname;
+                string email = choose.Email;
+                string phone = choose.Phone;
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    query = query.Where(x => x.Name.Contains(name));
+                }
+                else if (!string.IsNullOrWhiteSpace(surname))
+                {
+                    query = query.Where(x => x.Surname.Contains(surname));
+                }
+                else if (!string.IsNullOrWhiteSpace(email))
+                {
+                    query = query.Where(x => x.Email.Contains(email));
+                }
+                else if (!string.IsNullOrWhiteSpace(phone))
+                {
+                    query = query.Where(x => x.Phone.Contains(phone));
+                }
+                else
+                {
+                    query = context.Users.AsQueryable();
+                }
+                skip = (page - 1) * pageSize;
+                query = query.Skip(skip).Take(10);
+                result = query.ToList();
+                if (collection == null)
+                {
+                    collection = new ObservableCollection<User>();
+                    dgUsers.ItemsSource = collection;
+                }
+                List<string> imageList = new List<string>();
+                if (result.Count == 0)
+                {
+                    MessageBox.Show("0 person founded");
+                }
+                for (int i = 0; i < result.Count; i++)
+                {
+                    collection.Add(new User() { Id = i + 1, Name = result[i].Name, Email = result[i].Email, Image = result[i].Image, Surname = result[i].Surname, Password = result[i].Password, Phone = result[i].Phone });
+                }
+            }
         }
     }
 }
